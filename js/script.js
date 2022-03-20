b72faa19551606f6a47ced814ee1764baceb45a9
DavidKahumbi <david.kahumbi@student.moringaschool,com>
2022-03-20 18:31:44 +0300

    Add new changes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 //Buisness Logic
 function myOrder (size, crust, toppings) {
-    this.name = size;
-    this.colors = crust;
-    this.age = toppings;
+    this.size= size;
+    this.crust = crust;
+    this.toppings = toppings;
   }  
 
-
-
+ var size = (small, medium, larger)
+ var crust = (stuffed, glutenFree, crispy)
+ var toppings= (olives, cheese, pepperoni)
 
  //user interface
  $(container).ready()  
