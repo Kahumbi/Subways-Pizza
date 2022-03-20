69a204bc4d838b7a9a31fee1d14c60af764b79c2
DavidKahumbi <david.kahumbi@student.moringaschool,com>
2022-03-21 02:01:19 +0300

    Add changes

--- js/script.js
+++ js/script.js
@@ -15,21 +15,18 @@ function pizza (size, crust, toppings, total, orderNo) {
     var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
     order = order + 1;
     grandTotal = grandTotal + total;
-});  
+ });  
+
+    var newPizza = new pizza(sizeOfPizza, toppingsOfPizza, crustOfPizza, total, order);
 
   pizza.prototype.fullOrder = function() {
     return this.size + " " + this.crust + " " + this.toppings;
   }
-  $("ul#myOrder").append("<li><span class='order'>" + newOrder.fullOrder() + "</span></li>");
+  $("ul#pizza").append("<li><span class='order'>" + newOrder.fullOrder() + "</span></li>");
 
 
 
-  var sizeOfPizza = $(".size option:selected").val();
-  var toppingsOfPizza = $(".toppings option:selected").val();
-  var crustOfPizza = $(".crust option:selected").val();
-  var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
-  var order = 1;
-  var grandTotal = 0;
+  
 
 
 
